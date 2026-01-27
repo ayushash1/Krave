d31ef1b6e6f75bdf1ae8b81c1c1d81709694f63a
ayushash1 <ayushashtikar95@gmail.com>
2026-01-27 13:09:20 +0530

    fixed MongoDB connection error

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const connectDB = async () => {
+    
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDB");
